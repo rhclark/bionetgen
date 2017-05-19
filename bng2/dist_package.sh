71e7aa35ad3bfb31481468b23ede8df25426ed85
Robert Clark <rhclark.pitt@gmail.com>
2017-05-19 16:01:35 -0400

    q
    q

--- bng2/dist_package.sh
+++ bng2/dist_package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #  Read the VERSION file and construct the name of the installation package.
-echo " PLATFORM_ENV = " ${BASH_ARGV[1]}
+echo " PLATFORM_ENV = " ${TRAVIS_OS_NAME}
 #
 #input="./bng2/VERSION"
 #while read -r var
