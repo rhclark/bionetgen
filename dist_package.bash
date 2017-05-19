6676b60d30ed14a424bc11fd8d91adb453321f65
Robert Clark <rhclark.pitt@gmail.com>
2017-05-19 13:56:35 -0400

    ww
    ww

---
+++ dist_package.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#  Read the VERSION file and construct the name of the installation package.
+echo " PLATFORM_ENV = " $PLATFORM_ENV
+#
+#input="./bng2/VERSION"
+#while read -r var
+#do
+#  vbase="BioNetGen-$var"
+#  vall=$vbase"-Linux.tar.gz" 
+#  curl -T $vall  -u roberthclark:P1ttsburgh ftp://ftp.midcapsignals.com/midcap/junk/
+#done < "$input"
